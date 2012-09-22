0108d3dd595d224000028c965e6aa56270bced87
Frank <francisco.licea@gmail.com>
2012-09-22 13:13:00 -0500

    Add simple interface to dispaly transition probabilites on a sentence

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include "markov.h"
 #include "debug.h"
 
+#define MAX_LINE 1023
+
 void shift(char *state, int c, char *next_state, int state_len){
 	memmove(next_state, state + 1, state_len - 1);
 	next_state[state_len - 1] = c;
@@ -39,9 +41,11 @@ int train_on_file(char *path, int state_len)
 		state = next_state;
 		next_state = tmp;
 	}
+	MC_calc_p();
 
 	free(state);
 	free(next_state);
+	fclose(f);
 
 	return 1;
 
@@ -54,15 +58,70 @@ error:
 	return 0;
 }
 
+int print_state_p(char *line, int state_len){
+	int i = 0;
+	char c = 0;
+	char *state = NULL, *next_state = NULL, *tmp = NULL;
+	MC_Transition *t = NULL;
+
+	state = calloc(state_len + 1, sizeof(char));
+	check_mem(state);
+	next_state = calloc(state_len + 1, sizeof(char));
+	check_mem(next_state);
+
+	for(i = 0 ; i < state_len ; i++){
+		state[i] = next_state[i] = ' ';	
+	}
+
+	for(i = 0 ; line[i] ; i++){
+		c = line[i];
+
+		if(iscntrl(c)) c = ' ';
+
+		shift(state, c, next_state, state_len);	
+		t = MC_lookup(state, next_state, 0);
+
+		if (NULL == t){
+			printf("P(%s, %s) = %f\n", state, next_state, 0.0f);	
+		} else {
+			printf("P(%s, %s) = %f\n", state, next_state, t->p);
+		}
+
+		tmp = state;
+		state = next_state;
+		next_state = tmp;
+	}
+
+	free(state);
+	free(next_state);
+
+	return 1;
+
+error:
+
+	if(state) free(state);
+	if(next_state) free(next_state);
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int r = 0;
+	char line[MAX_LINE+1];
 
 	r = MC_init(3);
 	assert(r && "MC_init failed");
 
 	r = train_on_file("corpus/hg1.txt", 3);
 	assert(r && "Train on file failed");
+
+	while((fgets(line, MAX_LINE, stdin)) != NULL){
+		r = print_state_p(line, 3);	
+		assert(r && "Failed to print state transition probabilities");
+	}
+
+	MC_destroy();
 
 	return 0;
 }
